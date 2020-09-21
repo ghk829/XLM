614394e2c50e83c33dd15ef6e5707e7dc6c97e04
ghk829 <ghk829@hanmail.net>
2020-09-21 13:48:58 +0900

    Domain Adaptation시 pretrained된 domain의 dico로 eval할 domain의 dico로 바꾼다.

---
+++ adapt_dico.py
@@ -0,0 +1,51 @@
+import argparse
+import os
+import torch
+
+
+def get_parser():
+    """
+    Generate a parameters parser.
+    """
+    # parse parameters
+    parser = argparse.ArgumentParser(description="Language transfer")
+
+    # main parameters
+    parser.add_argument("--eval_domain", type=str, default="subtitles",
+                        help="Experiment dump path")
+
+    parser.add_argument("--pretrained_domain", type=str, default="koran",
+                        help="Experiment dump path")
+
+    return parser
+
+
+if __name__ == '__main__':
+    parser = get_parser()
+    params = parser.parse_args()
+
+    params.eval_domain
+
+    pretrained_domain_path = os.path.join('data/processed/de-en', params.pretrained_domain)
+    src_pth = os.path.join(pretrained_domain_path, 'valid.de-en.de.pth')
+    tgt_pth = os.path.join(pretrained_domain_path, 'valid.de-en.en.pth')
+
+    enc_dico = torch.load(src_pth)['dico']
+    dec_dico = torch.load(tgt_pth)['dico']
+
+    # torch.save(enc_dico, os.path.join(params.eval_domain, 'enc_dico.pth'))
+    # torch.save(dec_dico, os.path.join(params.eval_domain, 'dec_dico.pth'))
+
+    pth_files = [f for f in os.listdir(params.eval_domain) if
+                 f.endswith('pth') and f not in ['enc_dico.pth', 'dec_dico.pth']]
+
+    for f in pth_files:
+
+        state = torch.load(f)
+
+        if f.endswith('de.pth'):
+            state['dico'] = enc_dico
+        else:
+            state['dico'] = dec_dico
+
+        torch.save(state, os.path.join(params.eval_domain, f))
