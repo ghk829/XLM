99a709afdb77a6090bf492aa9048aa820fe09416
ghk829 <ghk829@hanmail.net>
2020-09-25 20:37:38 +0900

    Coverage Test를 위한 스크립트 작성

---
+++ coverage_test.py
@@ -0,0 +1,18 @@
+import torch
+
+
+
+if __name__ == '__main__':
+
+    domains = ['subtitles','koran','it','emea','acquis']
+
+    for src_domain in domains:
+        for tat_domain in domains:
+            for lg in ['de','en']:
+                data = torch.load(f'{src_domain}-{tat_domain}.train.de-en.{lg}.pth')
+                coverage = 1- 100. * sum(data['unk_words'].values()) / (len(data['sentences']) - len(data['positions']))
+                print("################### COVERAGE ###########################")
+                print(f'{src_domain}-{tat_domain}-{lg}')
+                print(coverage)
+                print('\n')
+
