fd37f16e76f991a59675417c9fb73b9766a4dc77
ghk829 <ghk829@hanmail.net>
2020-10-06 20:30:14 +0900

    head가 gamma만큼 작아지는 경우가 있는 경우 deactivate함 -> 잘못된 커밋

--- src/trainer.py
+++ src/trainer.py
@@ -848,15 +848,22 @@ class EncDecTrainer(Trainer):
         # cuda
         x1, len1, langs1, x2, len2, langs2, y = to_cuda(x1, len1, langs1, x2, len2, langs2, y)
 
+        reg_loss = 0
         # encode source sentence
         if params.l0_weight != 0:
-            enc1, reg_loss = self.encoder('fwd', x=x1, lengths=len1, langs=langs1, causal=False)
+            enc1, _reg_loss = self.encoder('fwd', x=x1, lengths=len1, langs=langs1, causal=False)
+            reg_loss+=_reg_loss.item()
         else:
             enc1 = self.encoder('fwd', x=x1, lengths=len1, langs=langs1, causal=False)
         enc1 = enc1.transpose(0, 1)
 
         # decode target sentence
-        dec2 = self.decoder('fwd', x=x2, lengths=len2, langs=langs2, causal=True, src_enc=enc1, src_len=len1)
+        if params.l0_weight != 0 and params.dec_self:
+            dec2, _reg_loss = self.decoder('fwd', x=x2, lengths=len2, langs=langs2, causal=True, src_enc=enc1, src_len=len1)
+            reg_loss += _reg_loss.item()
+        else:
+            dec2 = self.decoder('fwd', x=x2, lengths=len2, langs=langs2, causal=True, src_enc=enc1, src_len=len1)
+
 
         # loss
         _, loss = self.decoder('predict', tensor=dec2, pred_mask=pred_mask, y=y, get_scores=False)
@@ -864,8 +871,7 @@ class EncDecTrainer(Trainer):
         loss = lambda_coeff * loss
 
         if params.l0_weight != 0:
-            reg_loss = params.l0_weight * reg_loss.item()
-            self.stats['l0-loss'].append(reg_loss)
+            self.stats['l0-loss'].append(params.l0_weight * reg_loss.item())
             loss +=reg_loss
 
         # optimize
