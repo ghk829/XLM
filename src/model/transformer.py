7a7d85b4b34c5e937fe305416e650cc3d66424ff
ghk829 <ghk829@hanmail.net>
2020-11-22 01:01:11 +0900

    Meta Train Test

--- src/model/transformer.py
+++ src/model/transformer.py
@@ -380,6 +380,7 @@ def new_fast_params(named_parameters,grads,update_rate):
         return g
     new_named_parameters = [ (n,p-update_rate*get_grad(g)) for (n,p), g in zip(named_parameters,grads) ]
     fast_params = construct_fast_params(new_named_parameters)
+    del new_named_parameters
     return fast_params
 
 import itertools
